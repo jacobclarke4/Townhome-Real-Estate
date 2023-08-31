7cfe9547386e6dde17ea97e6f5a3d8d5cffa99e5
jacobclarke4 <125521732+jacobclarke4@users.noreply.github.com>
2023-08-31 15:34:25 -0500

    Update contact-me.js

--- contact-me.js
+++ contact-me.js
@@ -1,4 +1,4 @@
-     /*
+        /*
         function buy() {
             var buy_button = document.getElementById('buy-button');
             var sell_button = document.getElementById('sell-button');
@@ -22,15 +22,18 @@
             sell_button.classList.remove('selected');
             // Get all elements with the class .md-form.template7
             var form1 = document.querySelectorAll('.md-form.template7 .mg-container')[0];
-                form1.style.display = "block";
+            var form1_parent = document.querySelectorAll('.md-form.template7')[0];
+
+                form1_parent.style.display = "block";
                 form1.style.visibility = "visible";
                 form1.style.opacity = "1";
 
 
             var form2 = document.querySelectorAll('.md-form.template7 .mg-container')[1];
+            var form2_parent = document.querySelectorAll('.md-form.template7')[1];
                 form2.style.visibility = "hidden";
                 form2.style.opacity = "0";
-                form2.style.display = "none";
+                form2_parent.style.display = "none";
         }
         function sell() {
             var buy_button = document.getElementById('buy-button');
@@ -40,12 +43,14 @@
             buy_button.classList.remove('selected');
 
             var form1 = document.querySelectorAll('.md-form.template7 .mg-container')[0];
+            var form1_parent = document.querySelectorAll('.md-form.template7')[0];
                 form1.style.visibility = "hidden";
                 form1.style.opacity = "0";
-                form1.style.display = "none";
+                form1_parent.style.display = "none";
 
             var form2 = document.querySelectorAll('.md-form.template7 .mg-container')[1];
-                form2.style.display = "block";
+            var form2_parent = document.querySelectorAll('.md-form.template7')[1];
+                form2_parent.style.display = "block";
                 form2.style.visibility = "visible"
                 form2.style.opacity = "1";
         }
