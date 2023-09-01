810abb8d13f8c9ec30a3099d69ba2de29985d43f
jacobclarke4 <125521732+jacobclarke4@users.noreply.github.com>
2023-09-01 16:10:16 -0500

    Update contact-me.js

--- contact-me.js
+++ contact-me.js
@@ -1,3 +1,19 @@
+    /*
+        function buy() {
+            var buy_button = document.getElementById('buy-button');
+            var sell_button = document.getElementById('sell-button');
+
+            buy_button.style.background = "rgba(184, 184, 184, 1)";
+            sell_button.style.background = "white";
+        }
+        function sell() {
+            var buy_button = document.getElementById('buy-button');
+            var sell_button = document.getElementById('sell-button');
+
+            sell_button.style.background = "rgba(184, 184, 184, 1)";
+            buy_button.style.background = "white";
+        }
+        */
         function buy() {
             var buy_button = document.getElementById('buy-button');
             var sell_button = document.getElementById('sell-button');
@@ -28,56 +44,13 @@
 
             var form1 = document.querySelectorAll('.md-form.template7 .mg-container')[0];
             var form1_parent = document.querySelectorAll('.md-form.template7')[0];
-                fadeOutEffectBuy();
-                fadeInEffectSell();
-        }
+                form1.style.visibility = "hidden";
+                form1.style.opacity = "0";
+                form1_parent.style.display = "none";
 
-        function fadeOutEffectBuy() {
-            var fadeTarget = document.querySelectorAll('.md-form.template7 .mg-container')[0];
-            var fadeTargetParent = document.querySelectorAll('.md-form.template7')[0];
-            var fadeEffect = setInterval(function () {
-                if(fadeTarget.style.opacity <= 0) {
-                    fadeTargetParent.style.display = "none";
-
-                }
-                if (!fadeTarget.style.opacity) {
-                    fadeTarget.style.opacity = 1;
-                }
-                if (fadeTarget.style.opacity > 0) {
-                    fadeTarget.style.opacity -= 0.2;
-                } else {
-                    clearInterval(fadeEffect);
-                }
-            }, 100);
-        }
-
-        function fadeInEffectSell() {
-            var fadeTarget = document.querySelectorAll('.md-form.template7 .mg-container')[1];
+            var form2 = document.querySelectorAll('.md-form.template7 .mg-container')[1];
             var form2_parent = document.querySelectorAll('.md-form.template7')[1];
-
-            form2_parent.style.display = "block";
-            fadeTarget.style.visibility = "visible"
-            fadeTarget.style.opacity = 0;
-
-            var fadeEffect = setInterval(function () {
-                if (fadeTarget.style.opacity < 1) {
-                    fadeTarget.style.opacity += 0.2;
-                } else {
-                    clearInterval(fadeEffect);
-                }
-            }, 100);
-        }
-
-        function fadeOutEffectSell() {
-            var fadeTarget = document.querySelectorAll('.md-form.template7 .mg-container')[1];
-            var fadeEffect = setInterval(function () {
-                if (!fadeTarget.style.opacity) {
-                    fadeTarget.style.opacity = 1;
-                }
-                if (fadeTarget.style.opacity > 0) {
-                    fadeTarget.style.opacity -= 0.1;
-                } else {
-                    clearInterval(fadeEffect);
-                }
-            }, 100);
+                form2_parent.style.display = "block";
+                form2.style.visibility = "visible"
+                form2.style.opacity = "1";
         }
