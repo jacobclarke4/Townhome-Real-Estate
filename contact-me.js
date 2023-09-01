ee2c679b734c79e48201a5d31bd1cd2cf740a723
jacobclarke4 <125521732+jacobclarke4@users.noreply.github.com>
2023-09-01 14:46:04 -0500

    Update contact-me.js

--- contact-me.js
+++ contact-me.js
@@ -1,20 +1,4 @@
-   /*
-        function buy() {
-            var buy_button = document.getElementById('buy-button');
-            var sell_button = document.getElementById('sell-button');
-
-            buy_button.style.background = "rgba(184, 184, 184, 1)";
-            sell_button.style.background = "white";
-        }
-        function sell() {
-            var buy_button = document.getElementById('buy-button');
-            var sell_button = document.getElementById('sell-button');
-
-            sell_button.style.background = "rgba(184, 184, 184, 1)";
-            buy_button.style.background = "white";
-        }
-        */
-        function buy() {
+function buy() {
             var buy_button = document.getElementById('buy-button');
             var sell_button = document.getElementById('sell-button');
 
@@ -45,18 +29,21 @@
             var form1 = document.querySelectorAll('.md-form.template7 .mg-container')[0];
             var form1_parent = document.querySelectorAll('.md-form.template7')[0];
                 fadeOutEffectBuy();
-                form1_parent.style.display = "none";
 
-            var form2 = document.querySelectorAll('.md-form.template7 .mg-container')[1];
-            var form2_parent = document.querySelectorAll('.md-form.template7')[1];
-                form2_parent.style.display = "block";
-                form2.style.visibility = "visible"
-                form2.style.opacity = "1";
         }
 
         function fadeOutEffectBuy() {
             var fadeTarget = document.querySelectorAll('.md-form.template7 .mg-container')[0];
+            var fadeTargetParent = document.querySelectorAll('.md-form.template7')[0];
             var fadeEffect = setInterval(function () {
+                if(fadeTarget.style.opacity <= 0) {
+                    fadeTargetParent.style.display = "none";
+                    var form2 = document.querySelectorAll('.md-form.template7 .mg-container')[1];
+                    var form2_parent = document.querySelectorAll('.md-form.template7')[1];
+                        form2_parent.style.display = "block";
+                        form2.style.visibility = "visible"
+                        form2.style.opacity = "1";
+                }
                 if (!fadeTarget.style.opacity) {
                     fadeTarget.style.opacity = 1;
                 }
